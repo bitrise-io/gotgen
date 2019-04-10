e4b5c30d4fe7f6f04c98c21d3d0aa71540ea0962
Viktor Benei <viktor.benei@gmail.com>
2019-04-10 19:23:22 +0200

    Config flag fix: made it "PersistentFlag" (#9)

--- cmd/root.go
+++ cmd/root.go
@@ -39,15 +39,7 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	// RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gotgen.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().StringVar(&gotgenConfigFileName, "config", "gg.conf.json", "gotgen config file (gg.conf.json) path")
+	RootCmd.PersistentFlags().StringVar(&gotgenConfigFileName, "config", "gg.conf.json", "gotgen config file (gg.conf.json) path")
 }
 
 // initConfig reads in config file and ENV variables if set.
